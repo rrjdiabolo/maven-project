0cba871386b706e1eec1c27d5ca7c7f9e027fb7b
rrjdiabolo <42359410+rrjdiabolo@users.noreply.github.com>
2018-08-13 16:44:39 -0600

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,12 +11,12 @@ public class Greeter {
   public Greeter() {
 
   }
-  
+
   /**
   * @param el nombre de una persona
   * @return cadena de texto
   */
-  
+
   //Add javadoc comment
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
